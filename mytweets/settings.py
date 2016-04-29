54f936a065b865a9b458cac44138361f79ea4189
flyerooo <wxlong@foxmail.com>
2016-04-29 10:18:17 +0800

    base.html 修改

--- mytweets/settings.py
+++ mytweets/settings.py
@@ -80,3 +80,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+os.path.join(
+os.path.dirname(__file__),
+'static',
+),
+)
+
+TEMPLATE_DIRS = (
+    BASE_DIR + '/templates/'
+    )
+TEMPLATE_LOADES = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader', 
+    )
